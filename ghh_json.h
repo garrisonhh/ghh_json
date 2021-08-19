76331f37311160b331d99afb85496ea9b6a4196c
garrisonhh <garrisonhh@pm.me>
2021-08-19 00:03:20 -0400

    number support, reorganized file

--- ghh_json.h
+++ ghh_json.h
@@ -2,11 +2,12 @@
 #define GHH_JSON_H
 
 #include <stddef.h>
+#include <stdio.h>
 
 typedef struct json_object {
 	struct json_object *next;
 
-	const char *key; // may or may not be null
+	const char *key; // NULL for root object and array items
 
 	enum json_object_type {
 		JSON_OBJECT,
@@ -39,33 +40,15 @@ void json_load_file(json_t *, const char *filepath);
 void json_unload(json_t *);
 
 void json_print(json_t *);
+void json_fprint(json_t *, FILE *file);
 
 #ifdef GHH_JSON_IMPL
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
-// memory control
-#ifndef JSON_MALLOC
-#define JSON_MALLOC(size) malloc(size)
-#endif
-#ifndef JSON_FREE
-#define JSON_FREE(size) free(size)
-#endif
-
-// size of stack-allocated file reading buffer
-#ifndef JSON_FREAD_BUF_SIZE
-#define JSON_FREAD_BUF_SIZE 1024
-#endif
-
-// size of each json_t allocator page
-#ifndef JSON_PAGE_SIZE
-#define JSON_PAGE_SIZE 4096
-#endif
-
-#define JSON_INIT_PAGE_CAP 8
+// errors + debugging =========================================================
 
 #ifdef JSON_DEBUG_INFO
 #define JSON_DEBUG(...) printf(__VA_ARGS__)
@@ -88,11 +71,57 @@ void json_print(json_t *);
 		exit(-1);\
 	} while (0)
 
-typedef struct json_parse_ctx {
+typedef struct json_ctx {
 	json_t *json;
 	const char *text;
 	size_t index;
-} json_parse_ctx_t;
+} json_ctx_t;
+
+static void json_contextual_error(json_ctx_t *ctx) {
+	// get line number and line index
+	size_t line = 1, line_index = 0;
+
+	for (size_t i = 0; i < ctx->index; ++i) {
+		if (ctx->text[i] == '\n') {
+			++line;
+			line_index = i + 1;
+		}
+	}
+
+	// get line length
+	size_t i = line_index;
+	int line_length = 0;
+
+	while (ctx->text[i] != '\n' && ctx->text[i] != '\0') {
+		++line_length;
+		++i;
+	}
+
+	// print formatted line data
+	printf("%6zu | %.*s\n", line, line_length, ctx->text + line_index);
+	printf("%6s | %*s\n", "", (int)(ctx->index - line_index) + 1, "^");
+}
+
+// memory =====================================================================
+
+#ifndef JSON_MALLOC
+#define JSON_MALLOC(size) malloc(size)
+#endif
+#ifndef JSON_FREE
+#define JSON_FREE(size) free(size)
+#endif
+
+// size of file reading buffer
+#ifndef JSON_FREAD_BUF_SIZE
+#define JSON_FREAD_BUF_SIZE 1024
+#endif
+
+// size of each json_t allocator page
+#ifndef JSON_PAGE_SIZE
+#define JSON_PAGE_SIZE 4096
+#endif
+
+#define JSON_INIT_PAGE_CAP 8
 
 // fat functions use fat pointers to track memory allocation with JSON_MALLOC
 // and JSON_FREE. this is useful for allocating things that aren't on a json_t
@@ -120,29 +149,29 @@ static void *json_fat_realloc(void *ptr, size_t size) {
 	return new_ptr;
 }
 
-static void json_contextual_error(json_parse_ctx_t *ctx) {
-	// get line number and line index
-	size_t line = 1, line_index = 0;
-
-	for (size_t i = 0; i < ctx->index; ++i) {
-		if (ctx->text[i] == '\n') {
-			++line;
-			line_index = i + 1;
+// allocates on a json_t stack
+static void *json_alloc(json_t *json, size_t size) {
+	// allocate new page when needed
+	if (json->used + size > JSON_PAGE_SIZE) {
+		// allocate more space for page ptrs when needed
+		if (++json->cur_page == json->page_cap) {
+			json->page_cap <<= 1;
+			json->pages = json_fat_realloc(
+				json->pages,
+				json->page_cap * sizeof(*json->pages)
+			);
 		}
+
+		json->pages[json->cur_page] = JSON_MALLOC(JSON_PAGE_SIZE);
+		json->used = 0;
 	}
 
-	// get line length
-	size_t i = line_index;
-	int line_length = 0;
+	// return page space
+	void *ptr = json->pages[json->cur_page] + json->used;
 
-	while (ctx->text[i] != '\n' && ctx->text[i] != '\0') {
-		++line_length;
-		++i;
-	}
+	json->used += size;
 
-	// print formatted line data
-	printf("%6zu | %.*s\n", line, line_length, ctx->text + line_index);
-	printf("%6s | %*s\n", "", (int)(ctx->index - line_index), "^");
+	return ptr;
 }
 
 // internal json_t initializer
@@ -179,7 +208,7 @@ void json_load_file(json_t *json, const char *filepath) {
 
     // read text through buffer
     char *text = NULL;
-    char buffer[JSON_FREAD_BUF_SIZE];
+    char *buffer = JSON_MALLOC(JSON_FREAD_BUF_SIZE);
     size_t text_len = 0;
     bool done = false;
 
@@ -207,39 +236,20 @@ void json_load_file(json_t *json, const char *filepath) {
 
     text[text_len - 1] = '\0';
 
-	JSON_DEBUG("--- text ---\n%s\n", text);
+	free(buffer);
+
+	JSON_DEBUG("read file\n");
 
 	// load and cleanup
 	json_load(json, text);
+	
+	JSON_DEBUG("loaded file\n");
 
 	json_fat_free(text);
     fclose(file);
 }
 
-// allocates on a json_t stack
-static void *json_alloc(json_t *json, size_t size) {
-	// allocate new page when needed
-	if (json->used + size > JSON_PAGE_SIZE) {
-		// allocate more space for page ptrs when needed
-		if (++json->cur_page == json->page_cap) {
-			json->page_cap <<= 1;
-			json->pages = json_fat_realloc(
-				json->pages,
-				json->page_cap * sizeof(*json->pages)
-			);
-		}
-
-		json->pages[json->cur_page] = JSON_MALLOC(JSON_PAGE_SIZE);
-		json->used = 0;
-	}
-
-	// return page space
-	void *ptr = json->pages[json->cur_page] + json->used;
-
-	json->used += size;
-
-	return ptr;
-}
+// parsing ====================================================================
 
 static bool json_is_whitespace(char ch) {
 	switch (ch) {
@@ -253,8 +263,12 @@ static bool json_is_whitespace(char ch) {
 	}
 }
 
+static inline bool json_is_digit(char ch) {
+	return ch >= '0' && ch <= '9';
+}
+
 // skip whitespace to start of next token
-static void json_next_token(json_parse_ctx_t *ctx) {
+static void json_next_token(json_ctx_t *ctx) {
 	while (1) {
 		if (!json_is_whitespace(ctx->text[ctx->index]))
 			return;
@@ -265,7 +279,7 @@ static void json_next_token(json_parse_ctx_t *ctx) {
 
 // compare next token with passed token
 static bool json_token_equals(
-	json_parse_ctx_t *ctx, const char *token, size_t length
+	json_ctx_t *ctx, const char *token, size_t length
 ) {
 	for (size_t i = 0; i < length; ++i)
 		if (ctx->text[ctx->index + i] != token[i])
@@ -276,7 +290,7 @@ static bool json_token_equals(
 
 // error if next token does not equal passed token, otherwise skip
 static void json_expect_token(
-	json_parse_ctx_t *ctx, const char *token, size_t length
+	json_ctx_t *ctx, const char *token, size_t length
 ) {
 	if (!json_token_equals(ctx, token, length))
 		JSON_CTX_ERROR(ctx, "unknown token, expected \"%s\"/\n", token);
@@ -286,7 +300,7 @@ static void json_expect_token(
 
 // error if next character is not a valid json string character, otherwise
 // return char and skip
-static char json_expect_str_char(json_parse_ctx_t *ctx) {
+static char json_expect_str_char(json_ctx_t *ctx) {
 	if (ctx->text[ctx->index] == '\\') {
 		// escape codes
 		char ch;
@@ -326,7 +340,7 @@ static char json_expect_str_char(json_parse_ctx_t *ctx) {
 }
 
 // return string allocated on ctx allocator if valid string, otherwise error
-static char *json_expect_string(json_parse_ctx_t *ctx) {
+static char *json_expect_string(json_ctx_t *ctx) {
 	// verify string and count string length 
 	if (ctx->text[ctx->index++] != '\"')
 		JSON_CTX_ERROR(ctx, "unknown token, expected string.\n");
@@ -362,11 +376,54 @@ static char *json_expect_string(json_parse_ctx_t *ctx) {
 	return str;
 }
 
-static json_object_t *json_expect_object(json_parse_ctx_t *, json_object_t *);
-static json_object_t *json_expect_array(json_parse_ctx_t *, json_object_t *);
+static double json_expect_number(json_ctx_t *ctx) {
+	// minus symbol
+	bool negative = ctx->text[ctx->index] == '-';
+
+	if (negative)
+		++ctx->index;
+
+	// integral component
+	double num = 0.0;
+
+	if (!json_is_digit(ctx->text[ctx->index]))
+		JSON_CTX_ERROR(ctx, "expected digit.\n");
+
+	while (json_is_digit(ctx->text[ctx->index])) {
+		num *= 10.0;
+		num += ctx->text[ctx->index++] - '0';
+	}
+
+	// fractional component
+	if (ctx->text[ctx->index] == '.') {
+		++ctx->index;
+
+		if (!json_is_digit(ctx->text[ctx->index]))
+			JSON_CTX_ERROR(ctx, "expected digit.\n");
+
+		double fract = 0.0;
+
+		while (json_is_digit(ctx->text[ctx->index])) {
+			fract += ctx->text[ctx->index++] - '0';
+			fract *= 0.1;
+		}
+
+		num += fract;
+	}
+
+	// exponential component
+	if (ctx->text[ctx->index] == 'e' || ctx->text[ctx->index] == 'E') {
+		// TODO
+	}
+
+	return num;
+}
+
+static json_object_t *json_expect_object(json_ctx_t *, json_object_t *);
+static json_object_t *json_expect_array(json_ctx_t *, json_object_t *);
 
 // fills object in with value
-static void json_expect_value(json_parse_ctx_t *ctx, json_object_t *obj) {
+static void json_expect_value(json_ctx_t *ctx, json_object_t *obj) {
 	switch (ctx->text[ctx->index]) {
 	case '{':
 		json_expect_object(ctx, obj);
@@ -398,12 +455,21 @@ static void json_expect_value(json_parse_ctx_t *ctx, json_object_t *obj) {
 		obj->type = JSON_NULL;
 
 		break;
-	default:
+	default:;
+		// could be number
+		if (json_is_digit(ctx->text[ctx->index])
+		 || ctx->text[ctx->index] == '-') {
+			obj->data.number = json_expect_number(ctx);
+			obj->type = JSON_NUMBER;
+
+			break;
+		}
+
 		JSON_CTX_ERROR(ctx, "unknown token, expected value.\n");
 	}
 }
 
-static json_object_t *json_object_create(json_parse_ctx_t *ctx) {
+static json_object_t *json_object_create(json_ctx_t *ctx) {
 	json_object_t *obj = json_alloc(ctx->json, sizeof(*obj));
 
 	obj->next = obj->data.objects = NULL;
@@ -412,7 +478,7 @@ static json_object_t *json_object_create(json_parse_ctx_t *ctx) {
 }
 
 static json_object_t *json_expect_array(
-	json_parse_ctx_t *ctx, json_object_t *obj
+	json_ctx_t *ctx, json_object_t *obj
 ) {
 	json_object_t **tail = &obj->data.objects;
 
@@ -448,7 +514,7 @@ static json_object_t *json_expect_array(
 }
 
 static json_object_t *json_expect_object(
-	json_parse_ctx_t *ctx, json_object_t *obj
+	json_ctx_t *ctx, json_object_t *obj
 ) {
 	json_object_t **tail = &obj->data.objects;
 
@@ -488,7 +554,7 @@ static json_object_t *json_expect_object(
 }
 
 static void json_parse(json_t *json, const char *text) {
-	json_parse_ctx_t ctx = {
+	json_ctx_t ctx = {
 		.json = json,
 		.text = text,
 		.index = 0
@@ -503,11 +569,17 @@ static void json_parse(json_t *json, const char *text) {
 		json_expect_object(&ctx, json->root);
 		json->root->type = JSON_OBJECT;
 
+		json_next_token(&ctx);
+		json_expect_token(&ctx, "", 1);
+
 		break;
 	case '[':
 		json->root = json_object_create(&ctx);
 		json_expect_array(&ctx, json->root);
 		json->root->type = JSON_ARRAY;
+
+		json_next_token(&ctx);
+		json_expect_token(&ctx, "", 1);
 
 		break;
 	case '\0': // empty json is still valid json
@@ -519,48 +591,74 @@ static void json_parse(json_t *json, const char *text) {
 	}
 }
 
-static void json_print_level(int level) {
-	printf("%*s", level << 2, "");
+static void json_fprint_level(FILE *file, int level) {
+	fprintf(file, "%*s", level << 2, "");
 }
 
-static void json_print_lower(json_object_t *obj, int level) {
+static void json_fprint_lower(FILE *file, json_object_t *obj, int level) {
 	// print start of collection
-	printf(obj->type == JSON_OBJECT ? "{\n" : "[\n");
+	fprintf(file, obj->type == JSON_OBJECT ? "{\n" : "[\n");
 
 	// print children
 	++level;
 
 	for (json_object_t *trav = obj->data.objects; trav; trav = trav->next) {
-		json_print_level(level);
+		json_fprint_level(file, level);
 
 		if (trav->key)
-			printf("\"%s\": ", trav->key);
+			fprintf(file, "\"%s\": ", trav->key);
 	
 		switch (trav->type) {
 		case JSON_OBJECT:
 		case JSON_ARRAY:
-			json_print_lower(trav, level);
+			json_fprint_lower(file, trav, level);
 
 			break;
 		case JSON_STRING:
-			printf("\"%s\"\n", trav->data.string);
+			fprintf(file, "\"%s\"", trav->data.string);
 
 			break;
-		default:
-			printf("%%UNHANDLED%%\n");
+		case JSON_NUMBER:
+			if ((long)trav->data.number == trav->data.number)
+				fprintf(file, "%ld", (long)trav->data.number);
+			else
+				fprintf(file, "%f", trav->data.number);
+
+			break;
+		case JSON_TRUE:
+			fprintf(file, "true");
+
+			break;
+		case JSON_FALSE:
+			fprintf(file, "false");
+
+			break;
+		case JSON_NULL:
+			fprintf(file, "null");
+
 			break;
 		}
+
+		if (trav->next)
+			fprintf(file, ",");
+
+		fprintf(file, "\n");
 	}
 
 	--level;
 
 	// print end of collection
-	json_print_level(level);
-	printf(obj->type == JSON_OBJECT ? "}\n" : "]\n");
+	json_fprint_level(file, level);
+	fprintf(file, obj->type == JSON_OBJECT ? "}" : "]");
+}
+
+void json_fprint(json_t *json, FILE *file) {
+	json_fprint_lower(file, json->root, 0);
+	fprintf(file, "\n");
 }
 
 void json_print(json_t *json) {
-	json_print_lower(json->root, 0);
+	json_fprint(json, stdout);
 }
 
 #endif // GHH_JSON_IMPL
